46aeb588c55be01fe39a3b1f5d8c3eabecc70a49
Shramee Srivastav <11048263+shramee@users.noreply.github.com>
2026-08-20 21:34:44 +0530

    feat: add gnark limbcomposition

---
+++ internal/limbcomposition/composition_test.go
@@ -0,0 +1,72 @@
+package limbs_test
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"reflect"
+	"testing"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark-crypto/field/babybear"
+	"github.com/consensys/gnark/std/math/emulated"
+	"github.com/consensys/gnark/std/math/emulated/emparams"
+	"github.com/consensys/gnark/test"
+	limbs "github.com/mistcash/grosh26/internal/limbcomposition"
+)
+
+func TestComposition(t *testing.T) {
+	testComposition[emparams.BN254Fp](t)
+	testComposition[emparams.Secp256k1Fp](t)
+	testComposition[emparams.BLS12377Fp](t)
+	testComposition[emparams.Goldilocks](t)
+}
+
+func testComposition[T emulated.FieldParams](t *testing.T) {
+	t.Helper()
+	assert := test.NewAssert(t)
+	var fp T
+
+	assert.Run(func(assert *test.Assert) {
+		nbLimbs, nbBits := emulated.GetEffectiveFieldParams[T](ecc.BN254.ScalarField())
+		n, err := rand.Int(rand.Reader, fp.Modulus())
+		if err != nil {
+			assert.FailNow("rand int", err)
+		}
+		res := make([]*big.Int, nbLimbs)
+		for i := range res {
+			res[i] = new(big.Int)
+		}
+		if err = limbs.Decompose(n, nbBits, res); err != nil {
+			assert.FailNow("decompose", err)
+		}
+		n2 := new(big.Int)
+		if err = limbs.Recompose(res, nbBits, n2); err != nil {
+			assert.FailNow("recompose", err)
+		}
+		if n2.Cmp(n) != 0 {
+			assert.FailNow("unequal")
+		}
+	}, fmt.Sprintf("%s/limb=%d", reflect.TypeOf(fp).Name(), fp.BitsPerLimb()))
+	nbLimbs, nbBits := emulated.GetEffectiveFieldParams[T](babybear.Modulus())
+	assert.Run(func(assert *test.Assert) {
+		n, err := rand.Int(rand.Reader, fp.Modulus())
+		if err != nil {
+			assert.FailNow("rand int", err)
+		}
+		res := make([]*big.Int, nbLimbs)
+		for i := range res {
+			res[i] = new(big.Int)
+		}
+		if err = limbs.Decompose(n, nbBits, res); err != nil {
+			assert.FailNow("decompose", err)
+		}
+		n2 := new(big.Int)
+		if err = limbs.Recompose(res, nbBits, n2); err != nil {
+			assert.FailNow("recompose", err)
+		}
+		if n2.Cmp(n) != 0 {
+			assert.FailNow("unequal")
+		}
+	}, fmt.Sprintf("smallfield/%s/limb=%d", reflect.TypeOf(fp).Name(), nbBits))
+}
