8db0cbf67edcdd910bd03b58a6c1c89673f7900b
Shramee Srivastav <11048263+shramee@users.noreply.github.com>
2026-08-30 23:49:23 +0530

    feat: add poseidon 2-to-1 compression preimage inner circuit
    Proves knowledge of A, B for which Digest = Compress(A, B), poseidon2's
    width-2 feed-forward compression. Carries no commitments of its own; this
    proof becomes the fixture the outer Verifier circuit consumes.
    
    Closes #6

---
+++ circuits/poseidon/circuit_test.go
@@ -0,0 +1,42 @@
+package poseidon_test
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	"github.com/consensys/gnark/test"
+
+	"github.com/mistcash/grosh26/circuits/poseidon"
+)
+
+func TestCircuitSolved(t *testing.T) {
+	assert := test.NewAssert(t)
+
+	assignment, err := poseidon.AssignRandom()
+	assert.NoError(err)
+	assert.NoError(test.IsSolved(&poseidon.Circuit{}, assignment, ecc.BN254.ScalarField()))
+}
+
+// TestCircuitRejectsWrongDigest makes sure the check is not vacuous: a digest
+// poseidon does not produce from (A, B) must not solve.
+func TestCircuitRejectsWrongDigest(t *testing.T) {
+	assert := test.NewAssert(t)
+
+	var a, b fr.Element
+	_, err := a.SetRandom()
+	assert.NoError(err)
+	_, err = b.SetRandom()
+	assert.NoError(err)
+
+	assignment, err := poseidon.Assign(&a, &b)
+	assert.NoError(err)
+
+	wrong, ok := assignment.Digest.(*big.Int)
+	assert.True(ok)
+	wrong = new(big.Int).Add(wrong, big.NewInt(1))
+	assignment.Digest = wrong
+
+	assert.Error(test.IsSolved(&poseidon.Circuit{}, assignment, ecc.BN254.ScalarField()))
+}
